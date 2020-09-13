a067a945cb560dd4728218c9ff5bbf05536d509d
Anton Lindgren <anton.lindgren@yolean.com>
2020-09-13 14:32:22 +0200

    You can now brush again, the callback seems to work properly with react as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import ScratchIt from './ScratchIt.min.js';
+
+import brush from './images/brush_big.png';
+import klaver from './images/tile_klaver.png'
+
 import './App.css';
 
+const REVEAL_PERCENT = 50;
+
 function Tile({ tile }) {
+  const ref = useRef();
+  const [isRevealed, setIsRevealed] = useState(false);
+
+  const onReveal = () => setIsRevealed(true);
+
+  useEffect(() => {
+    const overlayImgUrl = klaver;
+    const brushImgUrl = brush;
+
+    ScratchIt(ref.current, overlayImgUrl, brushImgUrl, onReveal, REVEAL_PERCENT);
+  }, [tile.id]);
+
   return (
-    <div className="grid__tile">{tile.id}</div>
+    <div ref={ref} className="grid__tile">
+      {isRevealed ? 'REVEALED' : null}
+      {tile.id}
+    </div>
   );
 }
 
 function Grid({ tiles }) {
   return (
     <div className="grid">
-      {tiles.map(tile => <Tile tile={tile}></Tile>)}
+      <img style={{ display: 'none' }} src={brush}></img>
+      <img style={{ display: 'none' }} src={klaver}></img>
+      {tiles.map((tile, i) => <Tile key={i} tile={tile}></Tile>)}
     </div>
   );
 }
