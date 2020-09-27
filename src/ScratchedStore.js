b0f02af9d483c60590a079718e4d98a7b486a2ab
Anton Lindgren <anton.lindgren@yolean.com>
2020-09-27 10:17:20 +0200

    Indenti using 2 spaces, not 4

--- src/ScratchedStore.js
+++ src/ScratchedStore.js
@@ -1,59 +1,59 @@
 
 
 export default class ScratchedState {
-
-    constructor(audioPlayer) {
-        this.audioPlayer = audioPlayer;
-        this._state = {
-            numberOfScratchedTiles: 0,
-            scratchedTiles: {},
-            winningSoundPlayed: false,
-            won: false
-        };
-        this.TOTAL_NUMBER_OF_TILES = 10;
+  
+  constructor(audioPlayer) {
+    this.audioPlayer = audioPlayer;
+    this._state = {
+      numberOfScratchedTiles: 0,
+      scratchedTiles: {},
+      winningSoundPlayed: false,
+      won: false
+    };
+    this.TOTAL_NUMBER_OF_TILES = 10;
+  }
+  
+  _setWon() {
+    // We need 3 of the same thing to win
+    this._state.won = Object.values(this._state.scratchedTiles).includes(3);
+  }
+  
+  hasWon() {
+    return this._state.won;
+  }
+  
+  hasScratchedAll(){
+    return this._state.numberOfScratchedTiles === this.TOTAL_NUMBER_OF_TILES;
+  }
+  
+  updateState(tileId, audioReference) {
+    this._state.numberOfScratchedTiles++;
+    
+    // Add the scratched tile to the state
+    if (tileId in this._state.scratchedTiles) {
+      this._state.scratchedTiles[tileId]++;
+    } else {
+      this._state.scratchedTiles[tileId] = 1;
     }
-
-    _setWon() {
-        // We need 3 of the same thing to win
-        this._state.won = Object.values(this._state.scratchedTiles).includes(3);
+    
+    // Check if we've won
+    this._setWon();
+    
+    const audioPlayer = this.audioPlayer;
+    
+    if (this.hasWon() && !this._state.winningSoundPlayed) {
+      this._state.winningSoundPlayed = true;
+      audioReference.addEventListener("ended", function(){
+        audioReference.currentTime = 0;
+        audioPlayer.playWin();
+      });
     }
-
-    hasWon() {
-        return this._state.won;
+    
+    if (this.hasScratchedAll() && !this.hasWon()) {
+      audioReference.addEventListener("ended", function(){
+        audioReference.currentTime = 0;
+        audioPlayer.playLost();
+      });
     }
-
-    hasScratchedAll(){
-        return this._state.numberOfScratchedTiles === this.TOTAL_NUMBER_OF_TILES;
-    }
-
-    updateState(tileId, audioReference) {
-        this._state.numberOfScratchedTiles++;
-
-        // Add the scratched tile to the state
-        if (tileId in this._state.scratchedTiles) {
-            this._state.scratchedTiles[tileId]++;
-        } else {
-            this._state.scratchedTiles[tileId] = 1;
-        }
-
-        // Check if we've won
-        this._setWon();
-
-        const audioPlayer = this.audioPlayer;
-
-        if (this.hasWon() && !this._state.winningSoundPlayed) {
-            this._state.winningSoundPlayed = true;
-            audioReference.addEventListener("ended", function(){
-                audioReference.currentTime = 0;
-                audioPlayer.playWin();
-           });
-        }
-
-        if (this.hasScratchedAll() && !this.hasWon()) {
-            audioReference.addEventListener("ended", function(){
-                audioReference.currentTime = 0;
-                audioPlayer.playLost();
-            });
-        }
-    }
+  }
 }
